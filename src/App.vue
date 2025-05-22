7c40c7d523178093b32fea113a2d1ab379a167bc
jpperron123 <jpperron123@hotmail.com>
2025-05-21 20:19:11 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<style scoped></style>
+
+<template>
+	<header>
+		<div class="wrapper">
+			<HelloWorld class="bg-red-500" msg="You did it!" />
+		</div>
+	</header>
+
+	<main>
+		<TheWelcome />
+	</main>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
